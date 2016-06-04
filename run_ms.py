8ec0c900a299937d158d912d53e1238c5afe76a6
Watal M. Iwasaki <heavy.watal@gmail.com>
2016-06-04 17:45:26 +0900

    Prepare ms simulation

--- run_ms.py
+++ run_ms.py
@@ -1,50 +1,49 @@
 #!/usr/bin/env python
 import math
+import os
 import sys
 import subprocess
 import json
 
-
-def old():
-    nrep = 1000000
-    N0 = 200000
-    nu2b = 0.00014057  # *N0 = 28.11335961
-    nu2f = 0.00114639  # *N0 = 229.27796913
-    T = 0.00012709
-    # T_abs = T * 2 * N0  #= 50.83583664
-    # nu2b = nu2f * exp(-alpha * T)
-    call(nrep, nu2b, nu2f, N0, T)
+import run_dadi
 
 
-def call(nrep, nu2b, nu2f, N0, T, **kwargs):
-    nsam = 32
+def make_cmd(nrep, nu2b, nu2f):
+    sample_sizes = [16, 16]
     L = 10000
-    theta = 0.008
-    T_abs = T * 2 * N0  # = 50.83583664
+    theta = run_dadi.theta_Florida
+    T_abs = run_dadi.T_split
+    N0 = run_dadi.N0
     T_ms = T_abs / (4 * N0)
     alpha = math.log(nu2f / nu2b) / T_ms
+    strn = 'N0: {}, N2b: {}, N2f: {}\n'.format(N0, nu2b * N0, nu2f * N0)
+    sys.stderr.write(strn)
 
-    cmd = ['ms', nsam, nrep]
+    cmd = ['ms', sum(sample_sizes), nrep]
     cmd.extend(['-t', theta * L])
-    cmd.extend(['-I', 2, 16, 16])
+    cmd.extend(['-I', 2] + sample_sizes)
     cmd.extend(['-n', 2, nu2f])
     cmd.extend(['-g', 2, alpha])
     cmd.extend(['-ej', T_ms, 2, 1])
     cmd.extend(['-eg', T_ms, 2, 0])
-    cmd = [str(x) for x in cmd]
-    sys.stderr.write(' '.join(cmd) + '\n')
-    subprocess.call(cmd)
+    return [str(x) for x in cmd]
 
 
 #########1#########2#########3#########4#########5#########6#########7#########
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('-n', '--nrep', type=int, default=1)
+    parser.add_argument('-n', '--dry-run', action='store_true')
+    parser.add_argument('-r', '--nrep', type=int, default=1)
     parser.add_argument('infile')
     args = parser.parse_args()
 
+    (root, ext) = os.path.splitext(args.infile)
+    mu = float(root.split('_')[1])
+    run_dadi.set_global(mu)
     with open(args.infile, 'r') as fin:
         params = json.load(fin)
-    print(params)
-    call(args.nrep, **params)
+    cmd = make_cmd(args.nrep, *params)
+    sys.stderr.write(' '.join(cmd) + '\n')
+    if not args.dry_run:
+        subprocess.call(cmd)
