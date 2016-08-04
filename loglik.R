7e9ef6d70093ec2a3a5aecd38158b20cd1d10fb2
Watal M. Iwasaki <heavy.watal@gmail.com>
2016-08-04 23:48:20 +0900

    Calc top-left and right-bottom

--- loglik.R
+++ loglik.R
@@ -4,16 +4,19 @@ N0 = 35000
     purrr::map_df(~{
       xy = .[[1]]
       tibble(nu2b= xy[1], nu2f= xy[2], loglik=.[[2]])
-    }) %>>% (?.)
+    }) %>>%
+    dplyr::mutate_at(vars(nu2b, nu2f), function(x) x * N0) %>>%
+    (?.)
 
 .max_lik_params = fromJSON('./popt-Acar20160203-minimal-loadpexh_1.00e-08.json') %>>%
     (tibble(nu2b= .[1], nu2f= .[2])) %>>%
     dplyr::mutate_at(vars(nu2b, nu2f), function(x) x * N0) %>>%
     (?.)
 
+max_loglik = max(.data$loglik)
+
 .p = .data %>>%
-dplyr::mutate_at(vars(nu2b, nu2f), function(x) x * N0) %>>%
-dplyr::filter(loglik > -3.2e5) %>>%
+dplyr::filter(loglik > 2 * max_loglik) %>>%
 ggplot(aes(x=nu2b, y=nu2f))+
 geom_raster(aes(fill=loglik))+
 scale_fill_gradient(name='log likelihood', low='#f0f0f0f0', high='#000000')+
@@ -27,3 +30,12 @@ theme(panel.grid.minor=element_blank())+
 theme(legend.position=c(1, 1), legend.justification=c(1, 1))
 .p
 ggsave('loglik_grey.png', .p, width=5, height=5)
+
+.data %>>%
+    dplyr::filter(nu2f == max(nu2f)) %>>%
+    dplyr::arrange(desc(loglik))
+
+.data %>>%
+    dplyr::filter(nu2f == min(nu2f)) %>>%
+    dplyr::arrange(desc(loglik))
+
