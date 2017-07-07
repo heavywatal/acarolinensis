7e8435344ff893d31d58e0e0b6c4faccaa6b42fc
Watal M. Iwasaki <heavy.watal@gmail.com>
2017-07-07 17:52:08 +0900

    Calculate sample size from data_dict

--- convert2fs.py
+++ convert2fs.py
@@ -6,13 +6,26 @@ import re
 import dadi
 
 
+def get_sample_size(data_dict):
+    result = dict()
+    for locus in data_dict.values():
+        for pop, freqs in locus['calls'].items():
+            result[pop] = max(sum(freqs), result.get(pop, 0))
+    return result
+
+
 def convert2fs(snp_file):
     """Parse the data file to generate the data dictionary
     """
     fs_file = re.sub(r'\..+$', '.fs', snp_file)
     data_dict = dadi.Misc.make_data_dict(snp_file)
-    pop_ids = ['Flo', 'Chi']
-    sample_sizes = [16, 16]
+    print('{} loci'.format(len(data_dict)))
+    counts = get_sample_size(data_dict)
+    pop_ids = sorted(counts.keys(), reverse=True)
+    print(pop_ids)
+    assert pop_ids == ['Flo', 'Chi']
+    sample_sizes = [counts[x] for x in pop_ids]
+    print(sample_sizes)
     freq_spectrum = dadi.Spectrum.from_data_dict(
         data_dict, pop_ids, sample_sizes, polarized=False)
     # polarized=False means folded=True
