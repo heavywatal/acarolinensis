e84a3adab7adb34b135b37604d6a4eeed9094075
Watal M. Iwasaki <heavy.watal@gmail.com>
2017-07-11 18:42:37 +0900

    Update R code

--- loglik.R
+++ loglik.R
@@ -1,41 +1,39 @@
+library(tidyverse)
 library(jsonlite)
+
 N0 = 35000
-.data = fromJSON('./Acar20160203-minimal_1.00e-08.json') %>>%
+.exhaustive = fromJSON('./Acar20170707-minimal_1.00e-08.json') %>%
     purrr::map_df(~{
       xy = .[[1]]
       tibble(nu2b= xy[1], nu2f= xy[2], loglik=.[[2]])
-    }) %>>%
-    dplyr::mutate_at(vars(nu2b, nu2f), function(x) x * N0) %>>%
-    (?.)
+    }) %>%
+    dplyr::mutate_at(vars(nu2b, nu2f), function(x) x * N0) %>%
+    dplyr::arrange(desc(loglik)) %>%
+    print()
 
-.max_lik_params = fromJSON('./popt-Acar20160203-minimal-loadpexh_1.00e-08.json') %>>%
-    (tibble(nu2b= .[1], nu2f= .[2])) %>>%
-    dplyr::mutate_at(vars(nu2b, nu2f), function(x) x * N0) %>>%
-    (?.)
+max_loglik = max(.exhaustive$loglik)
 
-max_loglik = max(.data$loglik)
+read_param_json = function(infile) {
+    fromJSON(infile) %>%
+    {tibble(nu2b= .[1], nu2f= .[2])} %>%
+    dplyr::mutate_at(vars(nu2b, nu2f), function(x) x * N0)
+}
 
-.p = .data %>>%
-dplyr::filter(loglik > 1.5 * max_loglik) %>>%
-ggplot(aes(x=nu2b, y=nu2f))+
-geom_raster(aes(fill=loglik))+
-scale_fill_gradient(name='log likelihood', low='#f0f0f0f0', high='#000000')+
-geom_point(data=.max_lik_params, colour='#ffffff', size=4)+
-scale_x_log10()+
-scale_y_log10()+
-coord_cartesian(expand=FALSE)+
-labs(x=expression(italic(N)["2b"]), y=expression(italic(N)['2f']))+
-theme_bw(base_family='sans')+
-theme(panel.grid.minor=element_blank())+
-theme(legend.position=c(1, 1), legend.justification=c(1, 1))
+.max_lik_params_opt = read_param_json('./popt-Acar20170707-minimal-loadpexh_1.00e-08.json') %>% print()
+
+.p = .exhaustive %>%
+    dplyr::filter(loglik > 1.2 * max_loglik) %>%
+    ggplot(aes(x=nu2b, y=nu2f))+
+    geom_raster(aes(fill=loglik))+
+    scale_fill_gradient(name='log likelihood', low='#f0f0f0f0', high='#000000')+
+    # geom_point(data=top_n(.exhaustive, 1, wt=loglik), colour='#ff9900', size=4)+
+    geom_point(data=.max_lik_params_opt, colour='#ffffff', size=4)+
+    scale_x_log10()+
+    scale_y_log10()+
+    coord_cartesian(expand=FALSE)+
+    labs(x=expression(italic(N)["2b"]), y=expression(italic(N)['2f']))+
+    theme_bw(base_family='sans')+
+    theme(panel.grid.minor=element_blank())+
+    theme(legend.position=c(1, 1), legend.justification=c(1, 1))
 .p
-ggsave('loglik_grey.png', .p, width=5, height=5)
-
-.data %>>%
-    dplyr::filter(nu2f == max(nu2f)) %>>%
-    dplyr::arrange(desc(loglik))
-
-.data %>>%
-    dplyr::filter(nu2f == min(nu2f)) %>>%
-    dplyr::arrange(desc(loglik))
-
+ggsave('loglik_grey-Acar20170707.png', .p, width=5, height=5)
--- nulldist.R
+++ nulldist.R
@@ -1,41 +1,41 @@
-library(pipeR)
-library(readr)
-library(tidyr)
-library(dplyr)
-library(ggplot2)
+library(tidyverse)
 
-ms_stats = readr::read_tsv('./stats-Acar20160203-minimal-1.00e-08.txt.gz') %>>% (?.)
+ms_stats = readr::read_tsv('./stats-Acar20170707-minimal_1.00e-08.tsv.gz') %>% print()
 
-tidy = ms_stats %>>%
-    dplyr::select(-matches('^S_|^Kst$')) %>>%
-    tidyr::gather(variable, value) %>>% (?.)
+tidy = ms_stats %>%
+    dplyr::select(-matches('^S_|^Kst$')) %>%
+    tidyr::gather(variable, value) %>%
+    tidyr::drop_na() %>%
+    print()
 
-.summary = tidy %>>%
-    dplyr::group_by(variable) %>>%
+.summary = tidy %>%
+    dplyr::group_by(variable) %>%
     dplyr::summarise(
         mean=mean(value, na.rm=TRUE),
         sd=sd(value, na.rm=TRUE),
         q001=quantile(value, 0.01, na.rm=TRUE),
-        q003=quantile(value, 0.03, na.rm=TRUE),
         q005=quantile(value, 0.05, na.rm=TRUE),
         q095=quantile(value, 0.95, na.rm=TRUE),
-        q097=quantile(value, 0.97, na.rm=TRUE),
-        q099=quantile(value, 0.99, na.rm=TRUE)) %>>% (?.)
+        q099=quantile(value, 0.99, na.rm=TRUE)) %>% print()
 readr::write_tsv(.summary, 'ms_summary.tsv')
 
 .tr = c(D_1= 'D[Florida]',
-        D_2= 'D[Chichi]',
+        D_2= 'D[Ogasawara]',
         pi_1= 'pi[Florida]',
-        pi_2= 'pi[Chichi]',
-        Fst= 'F[ST]')
+        pi_2= 'pi[Ogasawara]',
+        tH_1= 'theta[Florida]',
+        tH_2= 'theta[Ogasawara]',
+        Fst= 'F[ST]',
+        Kst= 'K[ST]')
 
 .labeller = function(labels, multi_line=TRUE) {
     labels$variable = .tr[labels$variable]
     label_parsed(labels, multi_line)
 }
 
-.p = tidy %>>% #sample_n(20000) %>>%
-    dplyr::filter(!str_detect(variable, '_1$')) %>>%
+.p = tidy %>% #sample_n(20000) %>>%
+    dplyr::filter(!str_detect(variable, '_1$')) %>%
+    dplyr::filter(!str_detect(variable, '^tH')) %>%
     ggplot(aes(value))+
     geom_histogram(bins=24)+
     geom_point(pch=17, aes(x=q005, y=-2000), dplyr::filter(.summary, grepl('^D_2', variable)))+
